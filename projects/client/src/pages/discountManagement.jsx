449ee5ac763bee79437ad5dd844f32eb92f600ac
Ryan Defryan <muhammaddefryan@gmail.com>
2023-04-12 21:55:03 +0700

    Deployment - 2023/04/12 21:45 (#12)
    * Nathan initial commit, foldering, backend tooling, env
    
    * Nathan initial commit, foldering, backend tooling, env
    
    * landing page done, profile page + edit profile data done
    
    * landing page done, profile page + edit profile data done
    
    * TOK-4 Login Page DONE, TOK-2 Register Page DONE, TOK-6 Forgot Password Page DONE
    
    * Fix hash.js
    
    * sales report done , dashboard admin layout done
    
    * error new fd gak dapet
    
    * Feature Sort by and Select Courier Not Running
    
    * Sprint 2 Nathan done
    
    * branch admin register done
    
    * Nathan sprint 3 done, sprint 4 Ganti UI landing page
    
    * fix error
    
    * admin login and stock history done
    
    * Feature Product Category Done, Cart Page, Checkout Page Still On Progress
    
    * Feature Product Category List Done, Cart Page And Checkout Page Still On Progress
    
    * Fix Problem Add to Cart
    
    * Fix problem at server
    
    * Fix Problem Newpass Page
    
    * Fix Problem userController
    
    * admin product list done
    
    * Sprint 4 done
    
    * test
    
    * Feature Checkout: Courier Done, Transaction Admin, Fix UI Design
    
    * minor ui fix, opencage proximity done
    
    * minor ui fix, opencage proximity done
    
    * update fitur
    
    * Fix Product Category Page, Router From Landing Page, Add Transaction from Checkout
    
    * Fix Bug
    
    * responsive footer, minor ui fix, discount management page for super user
    
    * minor ui change
    
    * Update inputCategorical.js
    
    * input data 1
    
    * Update deploy.config.js
    
    * final push
    
    * Feature Transaction Admin DONE
    
    * Fix Problem On Super Admin Transaction
    
    * Fix payment proof page
    
    * Fix Eslint
    
    * Fix Transaction User
    
    * Fix Transaction Page User
    
    * minor update
    
    * My message
    
    * not done
    
    * update controller admin status
    
    * Fix Status Active Product
    
    * Fix Data tidak muncul
    
    * responsive minor bug
    
    * delete limit : 10 product controller get suggested
    
    * Fix Problem in Page Product Category and Register Page
    
    * debug final
    
    * debug done
    
    * Fix Minor Error
    
    * debug GR
    
    * Discount management bug fixed
    
    * fix error datepicker + conditional di backend
    
    * salah warna button datepicker mundur 1 bulan
    
    * udah gaada error plis capek saya
    
    ---------
    
    Co-authored-by: veanusnathan <veanusnathan.work@gmail.com>
    Co-authored-by: Veanus Nathan <116778569+veanusnathan@users.noreply.github.com>
    Co-authored-by: Aswin_Wibowo <aswin05.aw@gmail.com>
    Co-authored-by: Aswin Wibowo <116648439+Winz05@users.noreply.github.com>
    Co-authored-by: ashfimzk <ashfimzk@gmail.com>
    Co-authored-by: ashfimzk <115017513+ashfimzk@users.noreply.github.com>

--- projects/client/src/pages/discountManagement.jsx
+++ projects/client/src/pages/discountManagement.jsx
@@ -547,10 +547,187 @@ export default function DiscountManagement(props) {
 									<h3 className="w-full">{value.expired}</h3>
 									<h3 className="w-full">{value.status}</h3>
 								</div>
-							);
-						})}
-					</div>
-				</div>
+							) : (
+								<div className="space-y-2 pt-4 max-w-screen-xl w-full">
+									{all?.map((value, index) => {
+										return (
+											<div
+												className="grid grid-cols-6 border-[2px] px-2 py-1 rounded-md"
+												key={index}
+											>
+												<h3 className="w-full">
+													{value.discount_id === 1
+														? "BUY 1 GET 1"
+														: value.discount_id === 2
+														? "Minimum Purchased"
+														: "Custom Discount"}
+												</h3>
+												<h3 className="w-full">{value.min_purchase}</h3>
+												<h3 className="w-full">{value.percent}</h3>
+												<h3 className="w-full">{value.product.name}</h3>
+												<h3 className="w-full">{value.expired}</h3>
+												<div className="flex space-x-2">
+													<button
+														onClick={() => approveDiscount(value.id)}
+														className="bg-[#0095da] px-2 rounded-md text-white"
+													>
+														Approve
+													</button>
+													<button
+														onClick={() => declineDiscount(value.id)}
+														className="bg-red-500 px-2 rounded-md text-white"
+													>
+														Decline
+													</button>
+												</div>
+											</div>
+										);
+									})}
+								</div>
+							)}
+						</div>
+					</Tabs.Item>
+					<Tabs.Item title="Active">
+						<div className="rounded-lg max-w-screen-xl relative">
+							<div className="sticky w-full max-w-screen-xl grid grid-cols-6 bg-[#0095da] text-white px-3 rounded-md text-lg py-2 z-40">
+								<Dropdown label="Type" inline={true}>
+									<Dropdown.Item onClick={() => sortBy("discount_id-asc")}>
+										Ascending
+									</Dropdown.Item>
+									<Dropdown.Item onClick={() => sortBy("discount_id-desc")}>
+										Descending
+									</Dropdown.Item>
+								</Dropdown>
+								<Dropdown label="Minimum Purchase" inline={true}>
+									<Dropdown.Item onClick={() => sortBy("min_purchase-asc")}>
+										Ascending
+									</Dropdown.Item>
+									<Dropdown.Item onClick={() => sortBy("min_purchase-desc")}>
+										Descending
+									</Dropdown.Item>
+								</Dropdown>
+								<Dropdown label="Percent(%)" inline={true}>
+									<Dropdown.Item onClick={() => sortBy("percent-asc")}>
+										Ascending
+									</Dropdown.Item>
+									<Dropdown.Item onClick={() => sortBy("percent-desc")}>
+										Descending
+									</Dropdown.Item>
+								</Dropdown>
+								<Dropdown label="Product" inline={true}>
+									<Dropdown.Item onClick={() => sortBy("asc", 1)}>
+										Ascending
+									</Dropdown.Item>
+									<Dropdown.Item onClick={() => sortBy("desc", 1)}>
+										Descending
+									</Dropdown.Item>
+								</Dropdown>
+								<Dropdown label="Expired" inline={true}>
+									<Dropdown.Item onClick={() => sortBy("expired-asc")}>
+										Ascending
+									</Dropdown.Item>
+									<Dropdown.Item onClick={() => sortBy("expired-desc")}>
+										Descending
+									</Dropdown.Item>
+								</Dropdown>
+								<p className="inline">Status</p>
+							</div>
+							<div className="space-y-2 pt-4 max-w-screen-xl w-full">
+								{all?.map((value, index) => {
+									return (
+										<div
+											className="grid grid-cols-6 border-[2px] px-2 py-1 rounded-md"
+											key={index}
+										>
+											<h3 className="w-full">
+												{value.discount_id === 1
+													? "BUY 1 GET 1"
+													: value.discount_id === 2
+													? "Minimum Purchased"
+													: "Custom Discount"}
+											</h3>
+											<h3 className="w-full">{value.min_purchase}</h3>
+											<h3 className="w-full">{value.percent}</h3>
+											<h3 className="w-full">{value.product.name}</h3>
+											<h3 className="w-full">{value.expired}</h3>
+											<h3 className="w-full">{value.status}</h3>
+										</div>
+									);
+								})}
+							</div>
+						</div>
+					</Tabs.Item>
+					<Tabs.Item title="Declined">
+						<div className="rounded-lg max-w-screen-xl relative">
+							<div className="sticky w-full max-w-screen-xl grid grid-cols-6 bg-[#0095da] text-white px-3 rounded-md text-lg py-2 z-40">
+								<Dropdown label="Type" inline={true}>
+									<Dropdown.Item onClick={() => sortBy("discount_id-asc")}>
+										Ascending
+									</Dropdown.Item>
+									<Dropdown.Item onClick={() => sortBy("discount_id-desc")}>
+										Descending
+									</Dropdown.Item>
+								</Dropdown>
+								<Dropdown label="Minimum Purchase" inline={true}>
+									<Dropdown.Item onClick={() => sortBy("min_purchase-asc")}>
+										Ascending
+									</Dropdown.Item>
+									<Dropdown.Item onClick={() => sortBy("min_purchase-desc")}>
+										Descending
+									</Dropdown.Item>
+								</Dropdown>
+								<Dropdown label="Percent(%)" inline={true}>
+									<Dropdown.Item onClick={() => sortBy("percent-asc")}>
+										Ascending
+									</Dropdown.Item>
+									<Dropdown.Item onClick={() => sortBy("percent-desc")}>
+										Descending
+									</Dropdown.Item>
+								</Dropdown>
+								<Dropdown label="Product" inline={true}>
+									<Dropdown.Item onClick={() => sortBy("asc", 1)}>
+										Ascending
+									</Dropdown.Item>
+									<Dropdown.Item onClick={() => sortBy("desc", 1)}>
+										Descending
+									</Dropdown.Item>
+								</Dropdown>
+								<Dropdown label="Expired" inline={true}>
+									<Dropdown.Item onClick={() => sortBy("expired-asc")}>
+										Ascending
+									</Dropdown.Item>
+									<Dropdown.Item onClick={() => sortBy("expired-desc")}>
+										Descending
+									</Dropdown.Item>
+								</Dropdown>
+								<p className="inline">Status</p>
+							</div>
+							<div className="space-y-2 pt-4 max-w-screen-xl w-full">
+								{all?.map((value, index) => {
+									return (
+										<div
+											className="grid grid-cols-6 border-[2px] px-2 py-1 rounded-md"
+											key={index}
+										>
+											<h3 className="w-full">
+												{value.discount_id === 1
+													? "BUY 1 GET 1"
+													: value.discount_id === 2
+													? "Minimum Purchased"
+													: "Custom Discount"}
+											</h3>
+											<h3 className="w-full">{value.min_purchase}</h3>
+											<h3 className="w-full">{value.percent}</h3>
+											<h3 className="w-full">{value.product.name}</h3>
+											<h3 className="w-full">{value.expired}</h3>
+											<h3 className="w-full">{value.status}</h3>
+										</div>
+									);
+								})}
+							</div>
+						</div>
+					</Tabs.Item>
+				</Tabs.Group>
 			</div>
 			<Modal
 				show={show.createDiscount}
@@ -573,7 +750,11 @@ export default function DiscountManagement(props) {
 								onChange={(e) => {
 									setnewDiscount({
 										...newDiscount,
-										expired: `${e.$y}-${e.$M}-${e.$D}`,
+										expired: `${e.$d.toLocaleString("id-ID", {
+											year: "numeric",
+										})}-${e.$d.toLocaleString("id-ID", {
+											month: "2-digit",
+										})}-${e.$d.toLocaleString("id-ID", { day: "2-digit" })}`,
 									});
 								}}
 								format="YYYY/MM/DD"
